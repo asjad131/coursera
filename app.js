17b122f9e88959b826e11161082d0b992d5b6d4d
asjad131 <asjad131@users.noreply.github.com>
2023-02-07 05:08:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function greet(firstName, lastname){
+
+    return 'Hello ' + firstName + ' ' + lastname;
+
+}
+
+console.log(greet('Asjad', 'Mohammad'));
+
+const square = function(x = 3){
+ return x*x;
+};
+
+console.log(square(9));
+
+val= window.location.hostname;
+
+console.log(val);
